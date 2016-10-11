01997707211edcf479fbe3dbda44e685f3003f36
Kv0the <elnombredelviento@gmail.com>
2016-10-11 12:58:03 +0200

    Flickering lights and sounds

---
+++ Assets/Horror Hospital Pack/Scripts/Blinking.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinking : MonoBehaviour
+{
+
+    public
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float factor = 10;
+        float rand = Random.value*factor;
+        if (rand < 0.7)
+        {
+            gameObject.GetComponent<Light>().enabled = false;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        else gameObject.GetComponent<Light>().enabled = true;
+    }
+}
