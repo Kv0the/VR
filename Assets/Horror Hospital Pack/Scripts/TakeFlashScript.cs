c90c80cfae5eafda0984c4ded268e1ee89cca253
Luis Delicado <l.delicado33@gmail.com>
2016-10-23 20:43:08 +0200

    Flashlight
    Acabado la linterna con cambio de intensidades

--- Assets/Horror Hospital Pack/Scripts/TakeFlashScript.cs
+++ Assets/Horror Hospital Pack/Scripts/TakeFlashScript.cs
@@ -6,6 +6,7 @@ public class TakeFlashScript : MonoBehaviour {
 	private bool isGrap = false;
 	public Transform positionFlash;
 	private GameObject player;
+	private int level = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +16,20 @@ public class TakeFlashScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (isGrap) {
+			transform.parent = positionFlash;
 			transform.position = positionFlash.position;
-			transform.rotation = player.transform.GetChild(0).transform.rotation * positionFlash.rotation;
-
+			transform.rotation = positionFlash.rotation;
+			if (Input.GetKeyDown (KeyCode.E)) {
+				level += 1;
+				GetComponentInChildren<Light> ().intensity = 2 * level;
+				if (level == 3)
+					level = 0;
+			}
 		}
 		if (Vector3.Distance (player.transform.position, transform.position) < 3f && Input.GetKeyDown (KeyCode.F)) {
 			isGrap = true;
 			GetComponent<Rigidbody> ().detectCollisions = false;
+			GetComponent<Rigidbody> ().isKinematic = false;
 		}
 	}
 }
