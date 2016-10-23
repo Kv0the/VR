ae0869c08fc6f59f9c61170669969ca74c89534f
Luis Delicado <l.delicado33@gmail.com>
2016-10-23 20:03:33 +0200

    add flashLight
    Añadida la linterna y arreglado las carpetas

---
+++ Assets/Horror Hospital Pack/Scripts/TakeFlashScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeFlashScript : MonoBehaviour {
+
+	private bool isGrap = false;
+	public Transform positionFlash;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("FPSController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isGrap) {
+			transform.position = positionFlash.position;
+			transform.rotation = player.transform.GetChild(0).transform.rotation * positionFlash.rotation;
+
+		}
+		if (Vector3.Distance (player.transform.position, transform.position) < 3f && Input.GetKeyDown (KeyCode.F)) {
+			isGrap = true;
+			GetComponent<Rigidbody> ().detectCollisions = false;
+		}
+	}
+}
